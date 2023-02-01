61b926885f1718578e1195b5792877b9f46a1a05
abboskhan <abbosxon28012004@gmail.com>
2023-02-01 17:37:30 +0500

    allala

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,9 +1,29 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DishOrderDeterminer {
-    public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
-        return null;
+    public static void main(String[] args) {
+        for (Integer integer : determineDishOrder(10, 3)) {
+            System.out.println(integer);
+        }
+    }
+    public static List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+        int index = 0;
+        while (!queue.isEmpty()) {
+            index = (index + everyDishNumberToEat - 1) % queue.size();
+            result.add(queue.remove(index));
+        }
+        return result;
     }
 }
